2b4e1e3639c5776dc0b96ad1ab5bc16ece48b016
monomonosu <pojinyan@yahoo.co.jp>
2023-07-01 22:57:35 +0900

    ReactQuery設定追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
