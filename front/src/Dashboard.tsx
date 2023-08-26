c1c36b7cc256f277cb3aa58a8f26b5b4ea9708a4
monomonosu <pojinyan@yahoo.co.jp>
2023-08-26 19:06:36 +0900

    認証済みであれば、ログインした自身の情報を表示できるようにした

--- front/src/Dashboard.tsx
+++ front/src/Dashboard.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Axios = axios.create({
+  withCredentials: true,
+});
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+}
 
 const DashboardPage = () => {
+  const [user,setUser] = useState<User>();
+  useEffect(()=>{
+    const fetchDate = async() => await Axios.get("http://localhost/api/user")
+    .then((res) => {
+      console.log(res);
+      setUser(res.data)
+    });
+
+    fetchDate();
+  },[])
   return (
     <div>
       <h1>ダッシュボード</h1>
+      <h2>ユーザー情報</h2>
+      {user && (
+        <div>
+          <h1>{user.name}</h1>
+          <h1>{user.email}</h1>
+        </div>
+      )}
     </div>
   );
 };
