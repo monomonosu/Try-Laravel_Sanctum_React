1d44efe228744f059e7fb8b85e244d736e0a7398
monomonosu <pojinyan@yahoo.co.jp>
2023-09-16 18:33:23 +0900

    認証されてない時にダッシュボードにアクセスするとリダイレクトさせるテスト

--- front/src/Dashboard.tsx
+++ front/src/Dashboard.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
 
 const Axios = axios.create({
   withCredentials: true,
@@ -13,17 +15,24 @@ type User = {
 
 const DashboardPage = () => {
   const [user,setUser] = useState<User>();
+  const [loading,setLoading] = useState(true);
+  const navigate = useNavigate();
   useEffect(()=>{
     const fetchDate = async() => await Axios.get("http://localhost/api/user")
     .then((res) => {
       console.log(res);
       setUser(res.data)
-    });
+      setLoading(false);
+    })
+    .catch((err) => {
+      navigate("/login");
+    })
 
     fetchDate();
-  },[])
+  },[navigate])
   return (
     <div>
+      {loading && <CircularProgress />}
       <h1>ダッシュボード</h1>
       <h2>ユーザー情報</h2>
       {user && (
